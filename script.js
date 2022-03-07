95fd0e9fba1168a71ab093b80485339ddfcce395
Morari Cosmin <cosminmorari99@yahoo.com>
2022-03-07 14:38:12 -0800

    make it better

---
+++ script.js
@@ -0,0 +1,8 @@
+/* typing animation*/
+
+var typed = new Typed(".typing", {
+    strings:["","passionate Front-End", "graduate of a Front-End course"],
+    typeSpeed:100,
+    BackSpeed: 60,
+    loop: true
+})
