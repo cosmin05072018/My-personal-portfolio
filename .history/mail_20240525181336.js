66d231ee22fbe3185dc09765892f353a65a4f331
Morari Cosmin <cosminmorari99@yahoo.com>
2024-05-25 18:19:40 +0300

    miljs

---
+++ .history/mail_20240525181336.js
@@ -0,0 +1,49 @@
+const form = document.querySelector("#form");
+const statusMessage = document.querySelector("#status");
+const fromName = document.querySelector("#name");
+const message = document.querySelector("#message");
+const email = document.querySelector("#email");
+const subject = document.querySelector("#subject");
+
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    console.log(message.value);
+    if (fromName.value && subject.value && message.value && email.value) {
+        sendEmail(fromName.value, subject.value, email.value, message.value);
+        fromName.value = "";
+        subject.value = "";
+        email.value = "";
+        message.value = "";
+    }
+});
+
+emailjs.init({
+    publicKey: "sZKwvZMizJk7yk5LL",
+    blockHeadless: true,
+    limitRate: {
+        id: "app",
+        throttle: 10000,
+    },
+});
+
+function sendEmail(fromName, subject, email, message) {
+    const templateParams = {
+        name: fromName,
+        subject: subject,
+        email: email,
+        message: message,
+    };
+
+    emailjs.send("service_b97yl2v", "template_o3hphch", templateParams).then(
+        (response) => {
+            console.log("SUCCESS!", response.status, response.text);
+            statusMessage.style.display = "block";
+            statusMessage.innerHTML = `<p>SUCCESS!, ${response.text}</p>`;
+        },
+        (error) => {
+            console.log("FAILED...", error);
+            statusMessage.style.display = "block";
+            statusMessage.innerHTML = `<p>FAILED..., ${error}</p>`;
+        }
+    );
+}
---
+++ .history/mail_20240525181554.js
@@ -0,0 +1,51 @@
+const form = document.querySelector("#form");
+const statusMessage = document.querySelector("#status");
+const fromName = document.querySelector("#name");
+const message = document.querySelector("#message");
+const email = document.querySelector("#email");
+const subject = document.querySelector("#subject");
+
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    if (fromName.value && subject.value && message.value && email.value) {
+        sendEmail(fromName.value, subject.value, email.value, message.value);
+        fromName.value = "";
+        subject.value = "";
+        email.value = "";
+        message.value = "";
+    }else{
+        statusMessage.style.display = "block";
+        statusMessage.innerHTML = "<p>All fields are required!!!</p>";
+    }
+});
+
+emailjs.init({
+    publicKey: "sZKwvZMizJk7yk5LL",
+    blockHeadless: true,
+    limitRate: {
+        id: "app",
+        throttle: 10000,
+    },
+});
+
+function sendEmail(fromName, subject, email, message) {
+    const templateParams = {
+        name: fromName,
+        subject: subject,
+        email: email,
+        message: message,
+    };
+
+    emailjs.send("service_p5okwbb", "template_o3hphch", templateParams).then(
+        (response) => {
+            console.log("SUCCESS!", response.status, response.text);
+            statusMessage.style.display = "block";
+            statusMessage.innerHTML = `<p>SUCCESS!, ${response.text}</p>`;
+        },
+        (error) => {
+            console.log("FAILED...", error);
+            statusMessage.style.display = "block";
+            statusMessage.innerHTML = `<p>FAILED..., ${error}</p>`;
+        }
+    );
+}
